90bf19ecf08b1c4b08f0c8d7e675e7bc89579f8b
Jonathan James <jjamesnwa1011@gmail.com>
2017-08-27 20:08:43 -0700

    Created new fragment in fragments.html for list-jobs.html, added search functionality in searchController, added a for loop in search.html to display search results

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -22,6 +22,51 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    // TODO #1 - Create handler to process search request and display results-DONE!
 
+
+   @RequestMapping(value = "results")
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        //load jobs
+       //create empty ArrayList for jobs that comply with searchTerm
+       ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+       ArrayList<HashMap<String, String>> found_jobs = new ArrayList<>();
+
+       searchTerm = searchTerm.toLowerCase();
+
+        if (searchType.equals("all")) {
+            //loop over jobs and then loop over each column looking for searchTerm...(nested for)
+            for(HashMap<String,String> row:jobs) {
+                for (String column : row.keySet()) {
+
+                    String columnValue = row.get(column);
+                    columnValue = columnValue.toLowerCase();
+
+                    if (columnValue.contains(searchTerm) && !found_jobs.contains(row)) {
+                        found_jobs.add(row);
+                        }
+                    }
+                }
+
+            model.addAttribute("columns",ListController.columnChoices);
+            model.addAttribute("displayJobs", found_jobs);
+            return "search";
+        } else {
+            //loop through each job, find searchType(category) value, if value contains searchTerm return job...
+            for (HashMap<String, String> row : jobs) {
+                String categoryValue = row.get(searchType);
+                categoryValue = categoryValue.toLowerCase();
+                //change equals() to contains() for more open search results...
+                if (categoryValue.contains(searchTerm)) {
+                    found_jobs.add(row);
+                }
+            }
+
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("displayJobs", found_jobs);
+            return "search";
+        }
+    }
 }
+
+
